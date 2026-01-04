b5f2c207d6c81d6a4633891690caa7a0f569555c
Velnam <2286337140@qq.com>
2026-01-04 08:45:13 +0000

    Algorithm7 Finished

--- exercises/algorithm/algorithm7.rs
+++ exercises/algorithm/algorithm7.rs
@@ -31,8 +31,12 @@ impl<T> Stack<T> {
 		self.size += 1;
 	}
 	fn pop(&mut self) -> Option<T> {
-		// TODO
-		None
+		if self.size==0{
+			None
+		}else{
+			self.size-=1;
+			self.data.pop()
+		}
 	}
 	fn peek(&self) -> Option<&T> {
 		if 0 == self.size {
@@ -101,8 +105,27 @@ impl<'a, T> Iterator for IterMut<'a, T> {
 
 fn bracket_match(bracket: &str) -> bool
 {
-	//TODO
-	true
+	let mut stack: Stack<char> =Stack::new();
+	for ch in bracket.chars(){
+		match ch{
+			'('|'['|'{'=>stack.push(ch),
+			')'|']'|'}'=>{
+				let top =match stack.pop(){
+					Some(t)=>t,
+					None=>return false,
+				};
+				let ok=matches!(
+					(top,ch),
+					('(',')')|('[',']')|('{','}')
+				);
+				if !ok{
+					return false;
+				}
+			}
+			_=>{}
+		}
+	}
+	stack.is_empty()
 }
 
 #[cfg(test)]
